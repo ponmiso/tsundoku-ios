58741c1a6846148648ccbe339585a51881ce271c
Kohei Araya <40538759+ponmiso@users.noreply.github.com>
2025-06-21 00:31:46 +0900

    Fix Swift Lint pointers #4

--- Tsundoku/Tsundoku/UI/TsundokuApp.swift
+++ Tsundoku/Tsundoku/UI/TsundokuApp.swift
@@ -5,7 +5,7 @@ import SwiftData
 struct TsundokuApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
-            Item.self,
+            Item.self
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
