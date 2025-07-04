8d88b2b78a89572f01494aedf4b736bc0007e611
Kohei Araya <40538759+ponmiso@users.noreply.github.com>
2025-07-04 18:04:56 +0900

    Add book details

---
+++ Tsundoku/Tsundoku/UI/BookDetailsView.swift
@@ -0,0 +1,69 @@
+import SwiftData
+import SwiftUI
+
+struct BookDetailsView: View {
+    @Environment(\.modelContext) private var modelContext
+    @Environment(\.presentationMode) var presentationMode
+
+    @State private var book: Book
+
+    @State private var title: String
+
+    @State private var isPresentedAlert = false
+    @State private var updateAlertDetails: UpdateAlertDetails?
+
+    init(_ book: Book) {
+        self.book = book
+
+        title = book.title
+    }
+
+    var body: some View {
+        NavigationStack {
+            VStack(alignment: .leading) {
+                Text("Title")
+                TextField("", text: $title)
+                    .textFieldStyle(.roundedBorder)
+                Spacer()
+            }
+            .padding()
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Update") {
+                        updateBook(title: title)
+                    }
+                }
+            }
+        }
+        .alert("Update failed", isPresented: $isPresentedAlert, presenting: updateAlertDetails) { _ in
+            Button("OK") {}
+        } message: { details in
+            Text(details.message)
+        }
+
+    }
+}
+
+extension BookDetailsView {
+    private func updateBook(title: String) {
+        book.title = title
+        do {
+            try modelContext.save()
+            presentationMode.wrappedValue.dismiss()
+        } catch {
+            updateAlertDetails = UpdateAlertDetails(message: "Please restart the application and try again.")
+            isPresentedAlert = true
+        }
+    }
+}
+
+struct UpdateAlertDetails: Identifiable {
+    let id = UUID()
+    let message: String
+}
+
+#Preview {
+    let book = Book(title: "Test Book")
+    return BookDetailsView(book)
+        .modelContainer(for: Book.self, inMemory: true)
+}
