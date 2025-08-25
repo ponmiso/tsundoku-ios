f41d19487f9673761adb1a2a9c79c9a090c2bbb1
Kohei Araya <40538759+ponmiso@users.noreply.github.com>
2025-08-25 15:37:37 +0900

    Commonize the process of acquiring file URLs for applications

--- Tsundoku/Tsundoku/Manager/BookImageFileManager.swift
+++ Tsundoku/Tsundoku/Manager/BookImageFileManager.swift
@@ -1,6 +1,18 @@
 import Foundation
 
 struct BookImageFileManager {
+    func fileURL(fileName: String, for directory: FileManager.SearchPathDirectory = .applicationSupportDirectory) throws -> URL {
+        let fileManager = FileManager.default
+        let documentsURL = try fileManager.url(
+            for: directory,
+            in: .userDomainMask,
+            appropriateFor: nil,
+            create: true
+        )
+        let destinationURL = documentsURL.appendingPathComponent(fileName)
+        return destinationURL
+    }
+
     func saveTempPhotosPickerItem(_ imageData: Data) async throws -> URL {
         let timestamp = Int(Date().timeIntervalSince1970 * 1000)
         let tempURL = FileManager.default.temporaryDirectory.appendingPathComponent("photo_\(timestamp).jpg")
@@ -9,22 +21,16 @@ struct BookImageFileManager {
     }
 
     func moveToFile(from tempURL: URL, for directory: FileManager.SearchPathDirectory = .applicationSupportDirectory) throws -> URL {
+        let fileURL = try fileURL(fileName: tempURL.lastPathComponent, for: directory)
+
         let fileManager = FileManager.default
-        let documentsURL = try fileManager.url(
-            for: directory,
-            in: .userDomainMask,
-            appropriateFor: nil,
-            create: true
-        )
-        let fileName = tempURL.lastPathComponent
-        let destinationURL = documentsURL.appendingPathComponent(fileName)
 
         // すでに存在していれば削除
-        if fileManager.fileExists(atPath: destinationURL.path) {
-            try fileManager.removeItem(at: destinationURL)
+        if fileManager.fileExists(atPath: fileURL.path) {
+            try fileManager.removeItem(at: fileURL)
         }
 
-        try fileManager.moveItem(at: tempURL, to: destinationURL)
-        return destinationURL
+        try fileManager.moveItem(at: tempURL, to: fileURL)
+        return fileURL
     }
 }
