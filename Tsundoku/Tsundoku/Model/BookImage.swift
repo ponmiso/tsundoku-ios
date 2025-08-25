482a6c715286af780e061a19a650c2aa080afe3b
Kohei Araya <40538759+ponmiso@users.noreply.github.com>
2025-08-25 16:11:27 +0900

    Obtain the image file path of the application each time

--- Tsundoku/Tsundoku/Model/BookImage.swift
+++ Tsundoku/Tsundoku/Model/BookImage.swift
@@ -4,3 +4,23 @@ enum BookImage: Equatable, Codable {
     case url(URL)
     case filePath(URL)
 }
+
+extension BookImage {
+    var existingURL: URL? {
+        switch self {
+        case let .url(url):
+            return url
+        case let .filePath(fileURL):
+            // パス内のUUIDが変わる可能性があるため、ファイルがなければアプリケーションディレクトリを探索する
+            // また、tmpディレクトリをそのまま表示させるために、元のファイルの存在チェックをしている
+            let fileManager = FileManager.default
+            return if fileManager.fileExists(atPath: fileURL.path) {
+                fileURL
+            } else if let appURL = try? BookImageFileManager().fileURL(fileName: fileURL.lastPathComponent), fileManager.fileExists(atPath: appURL.path) {
+                appURL
+            } else {
+                nil
+            }
+        }
+    }
+}
