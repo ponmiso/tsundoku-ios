f3c37750ee903d4f062314ac8055df368892a0eb
Kohei Araya <40538759+ponmiso@users.noreply.github.com>
2025-09-02 16:55:19 +0900

    Conforms to RawRepresentable for AssociatedValue

--- Tsundoku/Tsundoku/Shortcut/ShortcutItem.swift
+++ Tsundoku/Tsundoku/Shortcut/ShortcutItem.swift
@@ -1,3 +1,22 @@
-enum ShortcutItem: String {
-    case add = "jp.ponmiso.Tsundoku.add"
+enum ShortcutItem {
+    case add
+}
+
+extension ShortcutItem: RawRepresentable {
+    typealias RawValue = String?
+
+    init?(rawValue: String?) {
+        switch rawValue {
+        case "jp.ponmiso.Tsundoku.add":
+            self = .add
+        default:
+            return nil
+        }
+    }
+
+    var rawValue: String? {
+        switch self {
+        case .add: return "jp.ponmiso.Tsundoku.add"
+        }
+    }
 }
